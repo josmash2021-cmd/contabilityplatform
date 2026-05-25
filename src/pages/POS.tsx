23f4171083d3a381acc98afb264752e29bc4ed14
Deploy <auto@deploy>
2026-05-25 15:19:04 +0800

    Move particle canvas to just below title bar, 70px height only

--- src/pages/POS.tsx
+++ src/pages/POS.tsx
@@ -393,7 +393,7 @@ function ParticleBackground() {
     window.addEventListener("resize", handleResize);
     return () => { cancelAnimationFrame(animId); window.removeEventListener("resize", handleResize); };
   }, []);
-  return <canvas ref={canvasRef} className="absolute top-0 left-0 right-0 h-[160px] pointer-events-none z-0" />;
+  return <canvas ref={canvasRef} className="absolute top-[56px] left-0 right-0 h-[70px] pointer-events-none z-0" />;
 }
 
 // Extracted Payment Dialog Component
